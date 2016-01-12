7bef09ef538490d80c4700bd03847c23b2f400ee
davidwhogg <david.hogg@nyu.edu>
2016-01-11 21:58:11 -0500

    lots more work

--- code/kmeans.py
+++ code/kmeans.py
@@ -30,27 +30,32 @@ if __name__ == "__main__":
                mg,          al,          s,           v,           mn,          ni) ).T
     labels = ["Fe",        "C",         "N",         "O",         "Na",
               "Mg",        "Al",        "S",         "V",         "Mn",        "Ni"]
-    ranges = [(-1.5, 0.5), (-0.6, 0.6), (-0.6, 0.6), (-0.3, 0.5), (-1.5, 1.0),
-              (-0.2, 0.6), (-1.5, 0.7), (-0.2, 0.6), (-1.5, 0.8), (-1.5, 1.0), (-1.5, 0.6)]
+    ranges = [(-1.5, 0.6), (-0.5, 0.6), (-0.7, 0.7), (-0.2, 0.5), (-2.0, 1.0),
+              (-0.2, 0.6), (-1.7, 0.8), (-0.2, 0.7), (-2.0, 0.9), (-2.0, 1.0), (-2.0, 0.7)]
     data = data[feh > -1.5] # according to MKN, but I don't believe her
     N, D = data.shape
     print(data.shape)
 
     print("running k-means")
-    km = cl.KMeans(n_clusters=16, random_state=42, n_init=1)
+    K = 256
+    km = cl.KMeans(n_clusters=K, random_state=42, n_init=32)
     clusters = km.fit_predict(data)
     centers = km.cluster_centers_.copy()
-    print(labels)
-    print(centers)
     print(centers.shape)
 
-    if True:
-        print("plotting corner")
+    for k in range(-1,K):
+        print("plotting corner", k)
         # corner.corner() order matters, apparently
-        figure = corner.corner(data, range=ranges, labels=labels, color="b",
-                               bins=128, plot_datapoints=True, plot_density=True, plot_contours=True)
-        corner.corner(centers, range=ranges, color="r", fig=figure,
-                      plot_datapoints=True, plot_density=False, plot_contours=False)
-        fn = "{}/centers.{}".format(dir, suffix)
-        figure.savefig(fn)
-        print(fn)
+        if k < 0:
+            subdata = data
+            fn = "{}/data.{}".format(dir, suffix)
+        else:
+            subdata = data[clusters == k]
+            fn = "{}/cluster_{:04d}.{}".format(dir, k, suffix)
+        print(subdata.shape)
+        N, D = subdata.shape
+        if N > D:
+            figure = corner.corner(subdata, range=ranges, labels=labels, color="k",
+                                   bins=128, plot_datapoints=True, plot_density=True, plot_contours=True)
+            figure.savefig(fn)
+            print(fn)
