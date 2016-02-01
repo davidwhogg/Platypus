5b75cfe5c11461bcc4061045e81b625c925b6afb
davidwhogg <david.hogg@nyu.edu>
2016-02-01 16:18:16 -0500

    outline

---
+++ code/clusters.py
@@ -0,0 +1,18 @@
+"""
+This file is part of the Platypus project.
+Copyright 2016 David W. Hogg (NYU)
+"""
+
+import numpy as np
+
+if __name__ == "__main__":
+    # read in cluster data
+    
+    # read in APOGEE / The Cannon data
+    # loop over clusters
+    # match data to cluster
+    # build background model
+    # build fg-bg model
+    # assign cluster membership
+    # measure abundances and trends
+    # output latex table
