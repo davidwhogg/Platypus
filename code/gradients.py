1609e9fe9e4d092db5c008ffdc0bbb238a3416aa
davidwhogg <david.hogg@nyu.edu>
2016-02-24 14:30:13 -0500

    compute x, y, z

--- code/gradients.py
+++ code/gradients.py
@@ -30,7 +30,8 @@ def get_data(fn, labels, metadata_labels):
     mask = ((table.field("TEFF_ASPCAP") > 3500.) *
             (table.field("TEFF_ASPCAP") < 5500.) *
             (table.field("LOGG_ASPCAP") > 0.) *
-            (table.field("LOGG_ASPCAP") < 3.9)) # MKN recommendation
+            (table.field("LOGG_ASPCAP") < 3.9) *
+            (table.field("DIST_Padova") > 0.))
     metadata = np.vstack((table.field(label) for label in metadata_labels)).T
     data = np.vstack((table.field(label) for label in labels)).T
     mask *= np.all(np.isfinite(data), axis=1)
@@ -42,11 +43,28 @@ def get_data(fn, labels, metadata_labels):
 if __name__ == "__main__":
     print("Hello World!")
 
-    data_labels = ["FE_H",
-                   "AL_H", "CA_H", "C_H", "K_H",  "MG_H", "MN_H", "NA_H",
-                   "NI_H", "N_H",  "O_H", "SI_H", "S_H",  "TI_H", "V_H"]
-    metadata_labels = ["APOGEE_ID", "RA", "DEC", "GLON", "GLAT", "VHELIO_AVG", "DM_Padova",
-                       "TEFF_ASPCAP", "LOGG_ASPCAP"]
+    data_labels = np.array(["FE_H",
+                            "AL_H", "CA_H", "C_H", "K_H",  "MG_H", "MN_H", "NA_H",
+                            "NI_H", "N_H",  "O_H", "SI_H", "S_H",  "TI_H", "V_H"])
+    metadata_labels = np.array(["APOGEE_ID", "RA", "DEC", "GLON", "GLAT", "VHELIO_AVG", "DIST_Padova",
+                                "TEFF_ASPCAP", "LOGG_ASPCAP"])
     dfn = "./data/cannon-distances.fits"
     data, metadata, mask = get_data(dfn, data_labels, metadata_labels)
     print(data.shape, metadata.shape, mask.shape)
+
+    # TO-DO: MOVE THIS LOGIC INTO get_data
+    distances = (metadata[:, metadata_labels == "DIST_Padova"]).flatten().astype(float)
+    ls = (np.pi / 180.) * (metadata[:, metadata_labels == "GLON"]).flatten().astype(float)
+    bs = (np.pi / 180.) * (metadata[:, metadata_labels == "GLAT"]).flatten().astype(float)
+    print(np.cos(1.2))
+    GXs = distances * np.cos(ls) * np.cos(bs) + 8.0 # kpc MAGIC
+    GYs = distances * np.sin(ls) * np.cos(bs)
+    GZs = distances * np.sin(bs)
+    GRs = np.sqrt(GXs ** 2 + GYs ** 2 + GZs ** 2)
+
+    # TO-DO: MAKE PRETTY PLOTS of slices
+    plt.clf()
+    plt.plot(GXs, GYs, "k.", alpha=0.25)
+    plt.savefig("foo.png")
+
+    # TO-DO: COMPUTE MEDIANS AND ROOT MEDIAN SQUARES OF ABUNDANCES in slices
